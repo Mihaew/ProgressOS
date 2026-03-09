638709ad13be20289ebbe86857784823e9a98dd2
Mihael <frankolmihael@gmail.com>
2026-03-09 11:09:42 +0100

    Početak projekta

--- progress-os/src/App.jsx
+++ progress-os/src/App.jsx
@@ -1,7 +1,32 @@
-const App = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
-};
+import { Router, Route, Navigate } from "@solidjs/router";
 
-export default App;
+import TestHomePage from "./TestHomePage";
+
+export default function App() {
+  return (
+    <Router root={Layout}>
+      <Route path="/" component={TestHomePage}></Route>
+      <Route path="*" component={NotFound} />
+    </Router>
+  );
+}
+
+function Layout(props) {
+  return (
+    <>
+      <div class="navbar bg-base-100 shadow-sm">
+        <div class="navbar-center">
+          <a href="/" class="btn btn-ghost text-xl">Progress OS</a>
+        </div>
+      </div>
+
+      <main class="min-h-[65vh] p-2">{props.children}</main>
+
+    </>
+  );
+}
+
+function NotFound() {
+  return <Navigate href="/error" state={{ error: { title: "404", message: "Tražena stranica ne postoji." } }} />
+}
+
---
+++ progress-os/src/TestHomePage.jsx
@@ -0,0 +1,5 @@
+export default function TestHomePage(){
+    return(
+        <div>TESTTTT</div>
+    );
+}
